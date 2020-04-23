2de5a024a8bbc2480778ebca9218f94482282480
Douglas Carvalho <dscarvalho199@gmail.com>
2020-04-23 15:59:54 -0300

    Commit Inicial

---
+++ src/ui/VideoList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Video from "./Video";
+
+export default function VideoList(props) {
+  const _list = [{ title: "", duration: "", url: "", cover: "" }];
+  return <ul className="list"></ul>;
+}
