ef48351a7accd7bcc015bede60a1d0d67c31ae9d
Douglas Carvalho <dscarvalho199@gmail.com>
2020-04-27 20:57:15 -0300

    isOnline

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
-import VideoList from './ui/VideoList';
-import NewVideoForm from './ui/NewVideoForm';
-import VideoPlayer from './ui/VideoPlayer';
-import {VideoContext} from './data/video/VideoContext';
+import VideoList from "./ui/VideoList";
+import NewVideoForm from "./ui/NewVideoForm";
+import VideoPlayer from "./ui/VideoPlayer";
+import { VideoContext } from "./data/video/VideoContext";
+import useOnlineStatus from "./data/hooks/useOnlineStatus";
 
 function App() {
+  const isOnline = useOnlineStatus();
+
+  useEffect(() => {
+    if (!isOnline) {
+      alert("Você desconectou!!");
+    }
+  }, [isOnline]);
   return (
     <div className="App">
+      {isOnline ? "Você está conectado" : "Você desconectou"}
       <VideoContext>
         <NewVideoForm />
         <VideoPlayer />
